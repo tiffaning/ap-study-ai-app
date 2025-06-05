ee9c73c1bc3b99b2698cb510327dd0cc66a9564c
tiffaning <tiffani.k.ng@gmail.com>
2025-06-05 18:12:09 -0400

    no change

--- backend/app/main.py
+++ backend/app/main.py
@@ -20,7 +20,7 @@ app.add_middleware(
 @app.get("/mcq")
 def get_mcq():
     return {
-        "question": "What is 2 + 5?",
+        "question": "What is 2 + 2?",
         "options": ["1", "2", "3", "4"],
         "answer": "4",
         "explanation": "2 + 2 equals 4."
